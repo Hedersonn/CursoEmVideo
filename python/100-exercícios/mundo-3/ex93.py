ef06af93c419da3797293efabb0688ad94c34038
Hedersonn <rigonatti.hederson@gmail.com>
2025-05-19 20:15:12 -0300

    +100 exercícios

--- python/100-exercícios/mundo-3/ex93.py
+++ python/100-exercícios/mundo-3/ex93.py
@@ -1,10 +1,15 @@
 #Crie um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do jogador e quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.
 
+
+# Valores
+
 jogador = {}
 gols = []
 
 jogador["Nome"] = input("Nome do jogador: ")
 partidas = int(input(f"Quantas partidas {jogador['Nome']} jogou? "))
+
+# Loop/ Valores finais
 
 for i in range(partidas):
     gols.append(int(input(f"Gols na partida {i + 1}: ")))
