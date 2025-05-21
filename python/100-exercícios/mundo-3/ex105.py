3c9a279e474e8896a19eb7dceade452011d58d82
Hedersonn <rigonatti.hederson@gmail.com>
2025-05-21 17:37:53 -0300

    +100 exercícios

---
+++ python/100-exercícios/mundo-3/ex105.py
@@ -0,0 +1,34 @@
+#Faça um programa que tenha uma função notas() que pode receber várias notas de alunos e vai retornar um dicionário com as seguintes informações:
+
+# Quantidade de notas
+# A maior nota
+# A menor nota
+# A média da turma
+# A situação (opcional)
+
+
+
+def notas(*notas, sit=False):
+    media = menor = maior = 0
+
+    resumo = {
+        "quantidade": len(notas),
+        "maior": max(notas),
+        "menor": min(notas),
+        "media": sum(notas) / len(notas)
+    }
+
+    if sit:
+        media = resumo["media"]
+        if media < 6:
+            resumo["situacao"] = "Ruim"
+        elif media < 7:
+            resumo["situacao"] = "Razoável"
+        else:
+            resumo["situacao"] = "Boa"
+
+    return resumo
+
+print(notas(2,5,3, sit=True))
+
+        
