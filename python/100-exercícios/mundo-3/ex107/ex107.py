03e4f88e0cd12cdbb8e9762f4d056a60c49aaa78
Hedersonn <rigonatti.hederson@gmail.com>
2025-05-24 10:57:17 -0300

    +100 exercícios

--- python/100-exercícios/mundo-3/ex107/ex107.py
+++ python/100-exercícios/mundo-3/ex107/ex107.py
@@ -1,8 +1,11 @@
+#Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() e metade(). Faça também um programa que importe esse módulo e use algumas dessas funções.
+
+
 import moeda
 
 v = float(input("Digite o valor em R$: "))
 
-print(f"O dobro de {v:.2f} e igual a {moeda.dobro(v)}")
-print(f"Metade de {v:.2f} e igual a {moeda.metade(v)}")
-print(f"Com acrescimo de 10% o valor {v:.2f} passou a valer {moeda.aumentar(v)}")
-print(f"Com desconto de 10% o valor de {v:.2f} passou a valer {moeda.diminuir(v)}")
+print(f"O dobro de R${v:.2f} e igual a R${moeda.dobro(v):.2f}")
+print(f"Metade de R${v:.2f} e igual a R${moeda.metade(v):.2f}")
+print(f"Com acrescimo de 5% o valor R${v:.2f} passou a valer R${moeda.aumentar(v, 5):.2f}")
+print(f"Com desconto de 10% o valor de R${v:.2f} passou a valer R${moeda.diminuir(v, 10):.2f}")
