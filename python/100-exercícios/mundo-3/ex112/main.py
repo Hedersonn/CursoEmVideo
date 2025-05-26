dd15228a00cf2b1f78d1eb2e4a58e7f31b726d60
Hedersonn <rigonatti.hederson@gmail.com>
2025-05-26 17:20:52 -0300

    +100 exercícios

--- python/100-exercícios/mundo-3/ex112/main.py
+++ python/100-exercícios/mundo-3/ex112/main.py
@@ -1,11 +1,13 @@
 # Dentro do pacote utilidadesCeV que criamos no desafio 111, temos um módulo chamado dado. Crie uma função chamada leiaDinheiro() que seja capaz de funcionar como a função imputa(), mas com uma validação de dados para aceitar apenas valores que seja monetários.
-
+from ex111.utilidades111 import moeda
 
 # Função do ex112
 
-def leiaDinheiro(valor=0):
-    valor = str(valor)
-    if valor.isalpha() or valor.isalnum() or valor.isspace() or valor.isacii():
-        print("Erro, digite um ")
+def leiaDinheiro(valor):
+    valor = str(valor).strip()
+    if valor.isnumeric():
+        return moeda.resumo(valor, 10, 5)
+    else:
+        return "Erro! Digite um valor válido!"
 
-leiaDinheiro()
+leiaDinheiro(input("Digite um número: "))
